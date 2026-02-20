239dc400a416782ffd094b22e5fff4454ae6017f
Wi-Fighter2006 <piyushchudhary97@gmail.com>
2026-02-20 08:27:42 +0530

    Define RGB pin mappings

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,21 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Piyush Choudhary
+ * @date 2026-02-20
  *
  * @details
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
-
- // TODO 2:
- // Define GREEN pin (Use 10)
-
- // TODO 3:
- // Define BLUE pin (Use 11)
+// STEP 1: Define RGB LED pins
+#define RED_PIN   9     // RED connected to pin 9
+#define GREEN_PIN 10    // GREEN connected to pin 10
+#define BLUE_PIN  11    // BLUE connected to pin 11
 
 void setup() {
-
-    // TODO 4:
-    // Initialize Serial communication (9600 baud)
-
-    // TODO 5:
-    // Configure RGB pins as OUTPUT
-
-    // TODO 6:
-    // Print initialization message
 }
 
 void loop() {
-
-    // -------- DIGITAL MODE --------
-
-    // TODO 7:
-    // Turn ON red (digital HIGH)
-
-    // TODO 8:
-    // Turn OFF red
-
-    // -------- ANALOG (PWM) MODE --------
-
-    // TODO 9:
-    // Set RED brightness using analogWrite()
-
-    // TODO 10:
-    // Set GREEN brightness using analogWrite()
-
-    // TODO 11:
-    // Set BLUE brightness using analogWrite()
-
-    // TODO 12:
-    // Add delay for visible transition
 }
